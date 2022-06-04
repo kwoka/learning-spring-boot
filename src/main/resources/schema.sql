bbce41f0060165545027fb3651c5c3364a9ceba4
Adam Kwok <adam.m.kwok@gmail.com>
2022-06-03 23:12:56 -0400

    Update application.properties to not use local MySQL db Fix data.sql and schema.sql to use h2 syntax.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,14 @@
 
 --Team Table
-CREATE TABLE `team`(
-	`team_id` tinyint(30) NOT NULL AUTO_INCREMENT,
-    `name` varchar(150) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    PRIMARY KEY(`team_id`));
+CREATE TABLE team(
+  team_id INT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  location VARCHAR(100) NOT NULL
+);
+
+-- Player Table
+CREATE TABLE player(
+  player_id INT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  age INT NOT NULL,
+  team_id INT NOT NULL);
